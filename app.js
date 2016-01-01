ce12a3c9139c28033c5920153bea7e8bdcf5d388
Eleven Chen <skyhacker@126.com>
2016-01-02 00:43:32 +0800

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+var app = {
+    generateShortId: function() {
+        return ("0000" + (Math.random()*Math.pow(36,4) << 0).toString(36)).slice(-4);
+    },
+    loadNavbar: function(){
+        $.ajax({
+            url: '/html/inc/navbar.html',
+            success: function(res) {
+                var template = Handlebars.compile(res);
+                var html = template();
+                $('body').prepend(html);
+            }
+        })
+    },
+    render:function(ele, source, data) {
+        template = Handlebars.compile(source);
+        $(ele).html(template(data));
+    }
+}
+
+$(document).ready(function(){
+    // app.loadHeader();
+    app.loadNavbar();
+});
